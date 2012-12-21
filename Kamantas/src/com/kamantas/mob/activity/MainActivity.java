d2c73469914a1852e56bd275325446cfc466f698
Mohamed Shalabi <mohamed.salah.shalabi@gmail.com>
2012-12-21 20:05:48 +0300

    Added login logic

--- Kamantas/src/com/kamantas/mob/activity/MainActivity.java
+++ Kamantas/src/com/kamantas/mob/activity/MainActivity.java
@@ -1,37 +1,231 @@
 package com.kamantas.mob.activity;
 
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kamantas.mob.R;
+import com.kamantas.mob.global.Globals;
+import com.kamantas.mob.util.http.HttpPostConnector;
+import com.kamantas.mob.util.http.IDataCallback;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements IDataCallback {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	private static final int LOGIN_REQUEST = 0;
+	private static final String TAG = null;
 
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private String mUsername;
+	private String mPassword;
+
+	private EditText mUsernameView;
+	private EditText mPasswordView;
+	private View mLoginFormView;
+	private View mLoginStatusView;
+	private TextView mLoginStatusMessageView;
+
+	private HttpPostConnector mConnector;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		mUsernameView = (EditText) findViewById(R.id.et_username);
+		mPasswordView = (EditText) findViewById(R.id.et_password);
+		mLoginFormView = findViewById(R.id.login_form);
+		mLoginStatusView = findViewById(R.id.login_status);
+		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+
+		mPasswordView
+				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+					@Override
+					public boolean onEditorAction(TextView textView, int id,
+							KeyEvent keyEvent) {
+						if (id == R.id.login || id == EditorInfo.IME_NULL) {
+							attemptLogin();
+							return true;
+						}
+						return false;
+					}
+				});
+
+		findViewById(R.id.btn_sign_in).setOnClickListener(
+				new OnClickListener() {
+					public void onClick(View v) {
+						attemptLogin();
+					}
+				});
+	}
+
+	protected void attemptLogin() {
+		if (mConnector != null) {
+			return;
+		}
+
+		// Reset errors.
+		mUsernameView.setError(null);
+		mPasswordView.setError(null);
+
+		// Store values at the time of the login attempt.
+		mUsername = mUsernameView.getText().toString();
+		mPassword = mPasswordView.getText().toString();
+
+		boolean cancel = false;
+		View focusView = null;
+
+		// Check for a valid password.
+		if (TextUtils.isEmpty(mPassword)) {
+			mPasswordView.setError(getString(R.string.error_field_required));
+			focusView = mPasswordView;
+			cancel = true;
+		} else if (mPassword.length() < 4) {
+			mPasswordView.setError(getString(R.string.error_invalid_password));
+			focusView = mPasswordView;
+			cancel = true;
+		}
+
+		// Check for a valid email address.
+		if (TextUtils.isEmpty(mUsername)) {
+			mUsernameView.setError(getString(R.string.error_field_required));
+			focusView = mUsernameView;
+			cancel = true;
+		} else if (!mUsername.contains("@")) {
+			mUsernameView.setError(getString(R.string.error_invalid_email));
+			focusView = mUsernameView;
+			cancel = true;
+		}
+
+		BasicNameValuePair pUsername = new BasicNameValuePair("username",
+				mUsername);
+		BasicNameValuePair pPassword = new BasicNameValuePair("password",
+				mPassword);
+
+		if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// Show a progress spinner, and kick off a background task to
+			// perform the user login attempt.
+			String loginUrl = getString(R.string.login_url);
+			Log.d("login", Globals.getInstance().getBaseUrl() + loginUrl);
+			mConnector = new HttpPostConnector(Globals.getInstance()
+					.getBaseUrl() + loginUrl, MainActivity.this, LOGIN_REQUEST,
+					MainActivity.this);
+
+			mConnector.execute(pUsername, pPassword);
+		}
+
+	}
+
+	/**
+	 * Shows the progress UI and hides the login form.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+	private void showProgress(final boolean show) {
+		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+		// for very easy animations. If available, use these APIs to fade-in
+		// the progress spinner.
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+			int shortAnimTime = getResources().getInteger(
+					android.R.integer.config_shortAnimTime);
+
+			mLoginStatusView.setVisibility(View.VISIBLE);
+			mLoginStatusView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 1 : 0)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginStatusView.setVisibility(show ? View.VISIBLE
+									: View.GONE);
+						}
+					});
+
+			mLoginFormView.setVisibility(View.VISIBLE);
+			mLoginFormView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 0 : 1)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginFormView.setVisibility(show ? View.GONE
+									: View.VISIBLE);
+						}
+					});
+		} else {
+			// The ViewPropertyAnimator APIs are not available, so simply show
+			// and hide the relevant UI components.
+			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void onPreExecute(int requestId, Activity ui) {
+		if (requestId == LOGIN_REQUEST) {
+			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			showProgress(true);
+			Toast.makeText(ui, "Signning in ...", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public void onPostExecute(int requestId, Activity ui, String response) {
+		if (requestId == LOGIN_REQUEST) {
+			mConnector = null;
+			showProgress(false);
+
+			JSONObject resp = null;
+			try {
+				resp = new JSONObject(response);
+
+				if ("0".equals(resp.getString("response"))) {
+					startActivity(new Intent("com.kamantas.TEAMS"));
+					finish();
+				} else {
+					mPasswordView
+							.setError(getString(R.string.error_incorrect_password));
+					mPasswordView.requestFocus();
+				}
+
+			} catch (JSONException e) {
+				Log.e(TAG, e.getMessage(), e);
+			}
+		}
+	}
 
 }
